58d3da72bea7d59309803f5e0aa5ec690acff3a6
Nicolas <kilian.nicolas@etudiant.univ-rennes1.fr>
2023-06-16 11:53:41 +0200

    first commit

---
+++ EX4_BDD.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		
+		<meta charset="utf-8">
+		<title>WEB2 TD12 EX4 : Formulaires & BDD ETUDIANTS</title>
+	</head>
+	<body>
+		<h1>WEB2 TD12 EX4 : Formulaires & BDD ETUDIANTS</h1>   
+		
+		<?php	
+			
+			
+			
+			
+			
+		?>
+		
+	</body>
+</html>
